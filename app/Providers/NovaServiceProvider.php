d7022aac0f8df14305baba0068addcae17589e5c
Joe Lohr <emailme@joelohr.com>
2020-02-23 18:15:07 -0600

    Added myself as a user for Nova

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -43,7 +43,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                //
+                'emailme@joelohr.com',
+                'deadlow@gmail.com'
             ]);
         });
     }
--- resources/views/livewire/post.blade.php
+++ resources/views/livewire/post.blade.php
@@ -6,11 +6,13 @@
                 <h4 class="card-title">{{ $post->title }}</h4>
                 <p class="text-grey">{{ \Carbon\Carbon::parse($post->created_at)->toFormattedDateString() }}</p>
             </div>
+            @if(!$post->body == '')
             <div class="card-content collapse show" aria-expanded="true">
                 <div class="card-body">
                     <p class="card-text">{!! $post->body !!}</p>
                 </div>
             </div>
+                @endif
         </div>
     @endforeach
 </div>
